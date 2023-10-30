d96ec88bfa6af4724a381aa37f4bd615687e20ed
RayoKh <platypus1703@gmail.com>
2023-10-30 02:39:30 +0200

    fixed

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,8 +3,9 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  [key: string]: any;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
 export {};
